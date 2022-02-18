57657574661f43865efec709c37074badac7cc47
rosh2001 <91378243+rosh2001@users.noreply.github.com>
2022-02-18 12:42:15 -0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,9 +1,10 @@
-import os
-from dotenv import load_dotenv
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, EmailField
+from wtforms.validators import DataRequired, Email
 
-load_dotenv()
 
-class Config(object):
-    """Base Config Object"""
-    DEBUG = True
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
+class ContactForm(FlaskForm):
+    name = StringField("Please enter your fullname", validators=[DataRequired()])
+    email = EmailField("Please enter your e-mail address", validators=[DataRequired(), Email()])
+    subject = StringField("Please enter the subject for your message", validators=[DataRequired()])
+    message = TextAreaField("Please enter the message you would like to send", validators=[DataRequired()], render_kw={"rows": "3"})
