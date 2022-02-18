8d2ff737b1278ffc30304508a4fa77a087f03c97
rosh2001 <91378243+rosh2001@users.noreply.github.com>
2022-02-18 12:48:46 -0500

    Update app.js

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,1 +1,11 @@
 /* Add your Application JavaScript */
+window.addEventListener('load', (event)=>{
+
+    setInterval( ()=>{
+        let flashcontainer= document.querySelector("span#flashitem");
+        if (document.contains(flashcontainer)){
+            flashcontainer.innerHTML="";
+            flashcontainer.classList.remove("success");
+        }
+    }, 3000)
+});
