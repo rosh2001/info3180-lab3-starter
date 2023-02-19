d8e9dc14860003773b06888630412f102449f4ad
rosh2001 <91378243+rosh2001@users.noreply.github.com>
2023-02-19 06:55:29 -0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -5,6 +5,6 @@ from wtforms.validators import DataRequired, Email
 
 class ContactForm(FlaskForm):
     name = StringField("Please enter your fullname", validators=[DataRequired()])
-    email = EmailField("Please enter your e-mail address", validators=[DataRequired(), Email()])
+    email = EmailField("Please enter your e-mail address", validators=[DataRequired(),Email()])
     subject = StringField("Please enter the subject for your message", validators=[DataRequired()])
     message = TextAreaField("Please enter the message you would like to send", validators=[DataRequired()], render_kw={"rows": "3"})
